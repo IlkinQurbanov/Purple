78f570dc7ace09152cd13802923f93e4f9418f33
IlkinQurbanov <ilkin.qurbanli.92@inbox.ru>
2023-11-13 15:31:24 +0400

    Update Recentwork

--- Purple/Areas/Admin/Controllers/RecentWorkController.cs
+++ Purple/Areas/Admin/Controllers/RecentWorkController.cs
@@ -36,11 +36,11 @@ namespace Purple.Areas.Admin.Controllers
         public async Task<IActionResult> Create(RecentWork recentWork)
         {
             if (!ModelState.IsValid) return View(recentWork);
-         
+
             //Eyni title olub olmadiqin yoxlamaq
             bool isExist =await _appDbContext.RecentWorks.AnyAsync(rcw => rcw.Title == recentWork.Title);
 
-            if(!isExist)
+            if(isExist)
             {
                 ModelState.AddModelError("Title", "We have this title");
                 return View(recentWork);
@@ -51,6 +51,80 @@ namespace Purple.Areas.Admin.Controllers
 
             return RedirectToAction("Index");
         }
+
+
+
+        //Update
+
+        [HttpGet]
+
+        public async Task<IActionResult> Update(int id)
+        {
+            var dbRcomp = await _appDbContext.RecentWorks.FindAsync(id);
+            if (dbRcomp == null) return NotFound();
+
+            return View(dbRcomp);
+
+        }
+
+        [HttpPost]
+
+            public async Task<IActionResult> Update(int id,RecentWork recentWork)
+        {
+            if (!ModelState.IsValid) return View(recentWork);
+
+            if (id != recentWork.Id) return BadRequest();
+
+            var dbRecentWork = await _appDbContext.RecentWorks.FindAsync(id);
+
+            if (dbRecentWork == null) return NotFound();
+
+            bool isExist = await _appDbContext.RecentWorks
+            .AnyAsync(rw => rw.Title.ToLower().Trim() == recentWork.Title.ToLower().Trim() && rw.Id != recentWork.Id);
+
+            if (isExist)
+            {
+                ModelState.AddModelError("Title", "We have this title");
+                return View(recentWork);
+
+            }
+
+            dbRecentWork.Title = recentWork.Title;
+            dbRecentWork.Desc = recentWork.Desc;
+            dbRecentWork.ImagePath = recentWork.ImagePath;
+
+            await _appDbContext.SaveChangesAsync();
+
+
+            return RedirectToAction("Index");
+
+        }
+
+
+        //Delete function
+
+        [HttpGet]
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var dbRcomp = await _appDbContext.RecentWorks.FindAsync();
+            if (dbRcomp == null) return NotFound();
+            return View(dbRcomp);
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteComponent(int id)
+        {
+            var dbRcomp = await _appDbContext.RecentWorks.FindAsync(id);
+            if (dbRcomp == null) return NotFound();
+            _appDbContext.RecentWorks.Remove(dbRcomp);
+            await _appDbContext.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+
+        }
+
 
     }
 }
--- Purple/Models/RecentWork.cs
+++ Purple/Models/RecentWork.cs
@@ -10,5 +10,6 @@ namespace Purple.Models
         public string Title { get; set; }
         public string Desc { get; set; }
         public string ImagePath { get; set; }
+   //     public bool isDeleted { get; set; } = false;
     }
 }
