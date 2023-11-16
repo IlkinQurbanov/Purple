e5a5e37af84812df55ba46152de25fe80c3211b1
IlkinQurbanov <ilkin.qurbanli.92@inbox.ru>
2023-11-16 11:13:22 +0400

    Complete update, delete, create

--- Purple/Areas/Admin/Controllers/ComponentController.cs
+++ Purple/Areas/Admin/Controllers/ComponentController.cs
@@ -7,6 +7,7 @@ using Purple.ViewModels.Work;
 
 namespace Purple.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class ComponentController : Controller
     {
         private readonly AppDbContext _dbContext;
@@ -23,7 +24,7 @@ namespace Purple.Areas.Admin.Controllers
 
         [HttpGet]
 
-        public async Task<IActionResult>  Create(WorkCreateViewModel component)
+        public async Task<IActionResult>  Create()
         {
             var categories = await _dbContext.Categories.ToListAsync();
             var selectedList = new List<SelectListItem>();
@@ -36,9 +37,40 @@ namespace Purple.Areas.Admin.Controllers
                 });
             }
 
-
-
-
+            var model = new WorkCreateViewModel
+            { 
+            
+            Items = selectedList
+            
+            };
+            return View(model);
         }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Create(WorkCreateViewModel component)
+        {
+
+
+            var component1 = new Models.Component();
+            component1.Title = component.Title;
+            component1.Desc = component.Desc;
+            component1.ImagePath = component.ImagePath;
+
+            foreach(var item in component.CategoryIds)
+            {
+                if(await _dbContext.Categories.FindAsync(item) != null)
+                {
+                    component1.CategoryComponents.Add(new CategoryComponent { CategoryId = item, ComponentId = component1.Id });
+                }
+            }
+
+
+           await _dbContext.Components.AddAsync(component1);
+            await _dbContext.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
     }
 }
